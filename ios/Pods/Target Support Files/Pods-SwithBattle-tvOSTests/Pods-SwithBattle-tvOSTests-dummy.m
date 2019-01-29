22775a812b45b2710e64cabfbe56838cbedd92a0
panoramix360 <lucasreis3000@gmail.com>
2019-01-29 17:30:43 -0200

    Config of firebase ios

---
+++ ios/Pods/Target Support Files/Pods-SwithBattle-tvOSTests/Pods-SwithBattle-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwithBattle_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_SwithBattle_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-SwithBattle/Pods-SwithBattle-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwithBattle : NSObject
+@end
+@implementation PodsDummy_Pods_SwithBattle
+@end

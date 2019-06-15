663b674af3cb22edb35d2a34bef0698e809f1104
lucas.reis <lucasreis3000@gmail.com>
2019-06-15 00:05:00 -0300

    Criando a primeira tela no projeto Unity

---
+++ Unity/Assets/UIScripts/UIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject tankButton;
+
+    private Animator tankButtonAnimator;
+
+    private void Start()
+    {
+        SetTankSettings();
+    }
+
+    private void SetTankSettings()
+    {
+        tankButtonAnimator = tankButton.GetComponent<Animator>();
+    }
+
+    public void onRankingClick()
+    {
+        Debug.Log("RankingClick");
+    }
+
+    public void onTankClick()
+    {
+        bool isLeft = tankButtonAnimator.GetBool("isLeft");
+
+        tankButtonAnimator.SetBool("isLeft", !isLeft);
+    }
+}

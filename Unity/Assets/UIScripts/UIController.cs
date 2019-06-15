6566ff3e35c8aa942521c1879ce259f10a9aae0c
lucas.reis <lucasreis3000@gmail.com>
2019-06-15 03:34:46 -0300

    Colocando o timer horário

--- Unity/Assets/UIScripts/UIController.cs
+++ Unity/Assets/UIScripts/UIController.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
     public GameObject tankButton;
+    public Text timerText;
 
+    private float timer = 3595;
     private Animator tankButtonAnimator;
 
     private void Start()
     {
         SetTankSettings();
+    }
+
+    private void Update()
+    {
+        if (timerText != null)
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    private void OnGUI()
+    {
+        float hour = Mathf.Floor((timer / 3600) % 24);
+        float minutes = Mathf.Floor((timer / 60) % 60);
+        float seconds = Mathf.RoundToInt(timer % 60) % 60;
+
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", hour, minutes, seconds);
     }
 
     private void SetTankSettings()
